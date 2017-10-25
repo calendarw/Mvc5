22df6a7bf9051de5fae26403bb8ba3d443063394
Kalun Wong <Kalun Wong>
2017-10-25 23:58:28 +0800

    Add project files.

---
+++ Mvc5/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Mvc5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Test(string text = null)
+        {
+            ViewBag.Param = text;
+            ViewBag.Dict = Request.Params;
+            return View();
+        }
+    }
+}
